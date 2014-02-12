e28f86dfa8c20366a3dea8d8f35daac28e882800
Himanshu Desai <himanshu.desai@icarehealth.com>
2014-02-13 10:34:15 +1100

    Adding script files/packages

---
+++ NLog.Targets.SignalR/NLog.Targets.SignalR.AspNetServer/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(NLog.Targets.SignalR.AspNetServer.Startup))]
+
+namespace NLog.Targets.SignalR.AspNetServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //Any connection or hub wire up and configuration should go here
+            //app.MapSignalR();
+            app.MapSignalR(new HubConfiguration
+            {
+                EnableDetailedErrors = true
+            });
+        }
+
+    }
+}
